86b7d3158f53227850a0ff833f115fb8ad84c74c
Cory Huebner <chuebner86@gmail.com>
2022-11-06 17:11:50 -0600

    bettered tests

--- src/exercises/001-types/types.test.ts
+++ src/exercises/001-types/types.test.ts
@@ -1,11 +1,11 @@
 import {
   makeAFalseBoolean,
   makeATrueBoolean,
-  makeAStringArray,
   makeAnObject,
   makeAWholeNumber,
   makeADecimalNumber,
   makeAString,
+  makeANumberArray,
   makeAnObjectArray,
 } from "./types";
 
@@ -19,7 +19,7 @@ describe("variables", () => {
   });
 
   it("should be able to make a decimal number between 1 and 1.5", () => {
-    const result = makeAWholeNumber();
+    const result = makeADecimalNumber();
     expect(result).toBeGreaterThanOrEqual(1.0);
     expect(result).toBeLessThanOrEqual(1.5);
   });
@@ -29,23 +29,24 @@ describe("variables", () => {
   });
 
   it("should be able to make a false boolean", () => {
-    expect(makeAFalseBoolean()).toBe(true);
+    expect(makeAFalseBoolean()).toBe(false);
   });
 
   it("should be able to make an object with the property woogie=boogie", () => {
     expect(makeAnObject()).toHaveProperty("woogie", "boogie");
   });
 
-  it("should be able to make a string array with at least 1, 3, and 5", () => {
-    expect(makeAStringArray).toContain([1, 3, 5]);
+  it("should be able to make a number array with at least 1, 3, and 5", () => {
+    expect(makeANumberArray()).toEqual(expect.arrayContaining([1, 3, 5]));
   });
 
-  it("should be able to make array with 2 objects. 1 object should be a dog=sophia whose favoriteFood=bacon", () => {
+  it("should be able to make array with 2 objects. 1 object should be have name=sophia whose favoriteFood=bacon", () => {
     const result = makeAnObjectArray();
     expect(result).toBeDefined();
     if (result) {
       expect(result).toHaveLength(2);
-      expect(result[0]).toEqual({ dog: "sophia", favoriteFood: "bacon" });
+      expect(result[0]).toHaveProperty("name", "sophia");
+      expect(result[0]).toHaveProperty("favoriteFood", "bacon");
     }
   });
 });
