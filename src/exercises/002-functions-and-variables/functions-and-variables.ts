a02a2bfbb78d888261cd2cdf3156e4073370822e
Cory Huebner <chuebner86@gmail.com>
2022-11-07 20:53:27 -0600

    added my solution for 002

--- src/exercises/002-functions-and-variables/functions-and-variables.ts
+++ src/exercises/002-functions-and-variables/functions-and-variables.ts
@@ -4,3 +4,10 @@ export const exampleConstNumber = 1234;
 export function exampleAddOne(value: number) {
   return value + 1;
 }
+
+export const hello = "Hello clammy boys!";
+export const murtle = { the: "turtle" };
+
+export function sayHello(name: string) {
+  return `Hello ${name}`;
+}
