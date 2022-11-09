fbc9d800c12348970a4ecbcc1677dd551ad82c69
Cory Huebner <chuebner86@gmail.com>
2022-11-08 20:47:01 -0600

    levi passing exercise 002

--- src/exercises/002-functions-and-variables/functions-and-variables.ts
+++ src/exercises/002-functions-and-variables/functions-and-variables.ts
@@ -4,3 +4,9 @@ export const exampleConstNumber = 1234;
 export function exampleAddOne(value: number) {
   return value + 1;
 }
+
+export const hello = "Hello Murtie";
+export const murtle = { the: "turtle" };
+export function sayHello(name: string) {
+  return "Hello " + name;
+}
