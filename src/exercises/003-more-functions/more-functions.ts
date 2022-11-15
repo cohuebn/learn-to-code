40659165cd04d9273951c803c784cc7aa28e0d0c
Cory Huebner <chuebner86@gmail.com>
2022-11-15 09:28:47 -0600

    passing test 1 of exercise 003

--- src/exercises/003-more-functions/more-functions.ts
+++ src/exercises/003-more-functions/more-functions.ts
@@ -1,1 +1,8 @@
 /* No functions! Add them here to pass the test */
+export function addThreeNumbers(
+  doggie1: number,
+  doggie3: number,
+  doggie4: number
+) {
+  return doggie1 + doggie3 + doggie4;
+}
